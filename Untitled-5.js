f8046366f4d34b8ee158065a06d604e207995a85
abhikumar9166905548-boop <abhikumar9166905548@gmail.com>
2026-02-28 15:22:37 +0530

    Delete Untitled-5.js

--- Untitled-5.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const commentSchema = new mongoose.Schema({
-reelId:String,
-user:String,
-text:String
-});
-
-module.exports = mongoose.model("Comment",commentSchema);
